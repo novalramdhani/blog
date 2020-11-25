bf5b8b518d68c8b6f6e2966e411f3a6990d8d5d6
novalramdhani <novalfitrahramdhani@gmail.com>
2020-11-26 06:16:43 +0700

    change directory app

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -10,16 +10,6 @@ class PostPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
     public function update(User $user, Post $post)
     {
         return $user->id === $post->user_id;
